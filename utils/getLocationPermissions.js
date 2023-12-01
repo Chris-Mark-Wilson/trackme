2dd4d37ec5e23915b7a11b0fa0c629f61ac415a5
chris-mark-wilson <cmwilson.co.uk>
2023-12-01 17:10:18 +0000

    modified permissions errors

--- utils/getLocationPermissions.js
+++ utils/getLocationPermissions.js
@@ -1,20 +1,24 @@
 import * as Location from 'expo-location';
 
-export const getLocationPermissions=async()=>{
-try{
-  let  statusFore  = await Location.requestForegroundPermissionsAsync();
-   let  statusBack  = await Location.requestBackgroundPermissionsAsync();
- 
-      if (statusFore.status !== 'granted' || statusBack.status !== 'granted') {
-        console.log('Permission to access location was denied');
-        console.log(statusFore,statusBack)
-        return  Promise.reject("Permission denied");;
-      }
+export const getLocationPermissions = async () => {
+  try {
+    let statusFore = await Location.requestForegroundPermissionsAsync();
+    let statusBack = await Location.requestBackgroundPermissionsAsync();
 
-     return "granted"
+    if (statusFore.status !== 'granted' && statusBack.status !== 'granted') {
+      console.log('Permission to access both foreground and background location was denied');
+      return Promise.reject("Both permissions denied");
+    } else if (statusFore.status !== 'granted') {
+      console.log('Permission to access foreground location was denied');
+      return Promise.reject("Foreground permission denied");
+    } else if (statusBack.status !== 'granted') {
+      console.log('Permission to access background location was denied');
+      return Promise.reject("Background permission denied");
     }
-    catch(error){
-      console.log(error,"error cannot get location");
-      return Promise.reject(error);
-    }
+
+    return "granted";
+  } catch (error) {
+    console.log(error, "error cannot get location");
+    return Promise.reject(error);
   }
+}
