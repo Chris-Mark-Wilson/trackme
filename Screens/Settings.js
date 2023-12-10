093f5f04c0fbdbe50ba602af6341dc402e2aeecf
chris-mark-wilson <cmwilson.co.uk>
2023-12-10 15:32:43 +0000

    tidied up settings screen

--- Screens/Settings.js
+++ Screens/Settings.js
@@ -10,23 +10,46 @@ export const Settings = ({ navigation }) => {
     <Text>Recording in progress, cannot change interval</Text>
   ) : (
     <>
-      <View>
-        <Text>Settings</Text>
-        <Text>Interval: {interval / 1000}seconds</Text>
+      <View
+        style={{
+          display: "flex",
+          alignItems: "center",
+          height: 400,
+          justifyContent: "space-around",
+       
+        }}
+      >
+        <Text style={{ fontSize: 20 }}> Timer Settings</Text>
+        <Text style={{ fontSize: 15 }}>
+          Point Interval: {interval / 1000} seconds
+        </Text>
         <Button
-          title="Increase Interval"
+          title="Increase Interval +1s"
           onPress={() => setInterval(interval + 1000)}
         ></Button>
         <Button
-          title="Decrease Interval"
+          title="Decrease Interval -1s"
           onPress={() => interval > 1000 && setInterval(interval - 1000)}
         ></Button>
-      </View>
-      <View>
         <Button
-          title="Go to Map"
-          onPress={() => navigation.navigate("Map")}
+          title="Increase Interval +1m"
+          onPress={() => setInterval(interval + 60000)}
         ></Button>
+        <Button
+          title="Decrease Interval -1m"
+          onPress={() => interval > 61000 ? setInterval(interval - 60000):setInterval(1000)}
+        ></Button>
+      </View>
+      <View
+        style={{
+          display: "flex",
+          alignItems: "center",
+          position: "absolute",
+          bottom: 50,
+          left: "40%",
+        }}
+      >
+        <Button title="Back" onPress={() => navigation.goBack()}></Button>
       </View>
     </>
   );
